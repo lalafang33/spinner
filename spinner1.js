ab08d74b504f6316ff2ab160b0ecf76c56a0c733
Lalafang33 <lalafang33@gmail.com>
2022-08-30 14:22:55 -0700

    second commit

--- spinner1.js
+++ spinner1.js
@@ -16,3 +16,23 @@ setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 800);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1200);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1400);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1600);
